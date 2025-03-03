15a6365b0b88584b01f1dc47ba28a265fd7f87db
Matthew Richardson <97010005+06tron@users.noreply.github.com>
2025-03-03 15:27:50 -0500

    Adds information from Piazza into the comments of BufferManager.java

--- buffer-manager/src/BufferManager.java
+++ buffer-manager/src/BufferManager.java
@@ -11,6 +11,18 @@ public abstract class BufferManager {
     /**
      * Fetches a page from memory if available; otherwise, loads it from disk.
      * The page is immediately pinned.
+     * 
+     * Piazza @40 - "You are right that in this specific use case with no
+     * updates or deletes a caller that invokes getPage will never mark it
+     * dirty."
+     * 
+     * Piazza @50 - "The file is only updated when dirty pages are evicted and
+     * written back by the buffer manager. You can only evict a page if its pin
+     * count is 0. The pin count is incremented for every invocation of
+     * getPage. You should evict the LRU page among the set of unpinned ones."
+     * 
+     * Piazza @62 - "A page is considered "used" when createPage or getPage is
+     * invoked on that page. Other events do not update the LRU status."
      *
      * @param pageId The ID of the page to fetch.
      * @return The Page object whose content is stored in a frame of the buffer
@@ -20,6 +32,12 @@ public abstract class BufferManager {
 
     /**
      * Creates a new page. The page is immediately pinned.
+     * 
+     * Piazza @54: "A page should be marked as dirty by the buffer manager
+     * inside the createPage method."
+     * 
+     * Piazza @65: The buffer should maintain an increasing counter of the
+     * previous pages to create new page IDs.
      *
      * @return The Page object whose content is stored in a frame of the buffer
      * pool manager.
@@ -29,6 +47,9 @@ public abstract class BufferManager {
     /**
      * Marks a page as dirty, indicating it needs to be written to disk before
      * eviction.
+     * 
+     * Piazza @54: Q - What happens if the caller uses markDirty on a page which
+     * is not in the buffer? A - "It would return an error."
      *
      * @param pageId The ID of the page to mark as dirty.
      */
@@ -36,6 +57,15 @@ public abstract class BufferManager {
 
     /**
      * Unpins a page in the buffer pool, allowing it to be evicted if necessary.
+     * 
+     * Piazza @50: "The buffer manager increments the pin count when a page is
+     * requested through the getPage or createPage method. It decrements the pin
+     * count when the caller invokes unpinPage, which indicates that the caller
+     * does not need the page any longer."
+     * 
+     * Piazza @54: Q - "When can we unpin the page?" A - "The caller invokes
+     * unpinPage when it is done using it. The buffer manager must be robust to
+     * the caller calling unpinPage at any time."
      *
      * @param pageId The ID of the page to unpin.
      */
