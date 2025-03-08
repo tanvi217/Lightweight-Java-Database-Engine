826e3ed81c7209a8b19678a74d4d4ecbab0a6a2b
Matthew Richardson <97010005+06tron@users.noreply.github.com>
2025-03-08 14:12:01 -0500

    Fixes IMDbPage to store nextRowId in the buffer, still to add more comments

--- buffer-manager/src/IMDbPage.java
+++ buffer-manager/src/IMDbPage.java
@@ -3,25 +3,36 @@ import java.nio.ByteBuffer;
 public class IMDbPage implements Page {
 
     private static int rowBytes = 39;
-    private int totalRows;
-    private int pageStart; // index of first byte of this page in buffer
-    private ByteBuffer buffer;
-    private int fullRows;
     private int pageId;
+    private ByteBuffer buffer;
+    private int maxRows;
+    private int pageStart; // index of first byte of this page in buffer
+    private int lastByteIndex;
     private Row[] rows;
+    private int nextRowId; // equal to the number of full rows in this page
 
-    public IMDbPage(int pageId, int pageBytes, ByteBuffer buffer) {
-        this.buffer = buffer;
+    private void setNextRowId(int rowId) {
+        nextRowId = rowId;
+        buffer.put(lastByteIndex, (byte) rowId);
+    }
+    
+    public IMDbPage(int pageId, int pageBytes, ByteBuffer buffer, boolean isEmpty) {
         this.pageId = pageId;
-        totalRows = pageBytes / rowBytes;
+        this.buffer = buffer;
+        maxRows = (pageBytes - 1) / rowBytes; // subtract one for nextRowId byte
         pageStart = pageId * pageBytes;
-        fullRows = 0;
-        rows = new Row[totalRows];
+        lastByteIndex = pageStart + pageBytes - 1;
+        rows = new Row[maxRows];
+        if (isEmpty) {
+            setNextRowId(0);
+        } else {
+            nextRowId = (int) buffer.get(lastByteIndex);
+        }
     }
 
     @Override
     public Row getRow(int rowId) {
-        if (rowId < 0 || rowId >= fullRows) {
+        if (rowId < 0 || rowId >= nextRowId) {
             throw new IllegalArgumentException("Row index out of bounds.");
         }
         if (rows[rowId] != null) {
@@ -38,21 +49,21 @@ public class IMDbPage implements Page {
 
     @Override
     public int insertRow(Row row) {
-        if (fullRows >= totalRows) {
-            return -1;
+        if (nextRowId >= maxRows) {
+            return -1; // I think it said to do this in project description
         }
-        int rowId = fullRows;
+        int rowId = nextRowId;
         int rowStart = pageStart + rowId * rowBytes;
         buffer.put(row.movieId, rowStart, rowStart + 9);
         buffer.put(row.title, rowStart + 9, rowStart + 39);
         rows[rowId] = row;
-        fullRows += 1;
+        setNextRowId(nextRowId + 1);
         return rowId;
     }
 
     @Override
     public boolean isFull() {
-        return fullRows >= totalRows;
+        return nextRowId >= maxRows;
     }
 
     @Override
