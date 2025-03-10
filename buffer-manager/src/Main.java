dd74acc15e831856567b25c636ec1b0ab8a0e4b1
Matthew Richardson <97010005+06tron@users.noreply.github.com>
2025-03-09 23:05:30 -0400

    Moves test from Main into test directory

--- buffer-manager/src/Main.java
+++ buffer-manager/src/Main.java
@@ -1,51 +1,9 @@
-import java.io.IOException;
-
 public class Main {
 
     public static void main(String[] args) {
         //experimented with different buffer sizes by changing Constants.BUFFER_SIZE
         BufferManager bufferManager = new LRUBufferManager(Constants.BUFFER_SIZE);
         Utilities.loadDataset(bufferManager, Constants.IMDB_FILE_PATH);
-        //bufferManagerLRUTests();
     }
 
-    private static void bufferManagerLRUTests(){
-        //This test is useful as we run on a small set of pages and make sure the basic functionality works
-        //We are able to add pages to the buffer manager, keep them in the frames, evict to add new ones, and write to disk
-        //The disk writes are all 0 as each page has not had any information added yet, but importantly in imdb_db.bin we can
-        //see that there are 6000 lines (in hex) i.e. 6 pages worth of space. We do this using the hex editor installed on vscode
-        int totalPages = 10;
-        int bufferSize = 4;
-        BufferManager bm = new LRUBufferManager(bufferSize, true);
-        for (int i = 0; i < totalPages; ++i) {
-            Page page = bm.createPage();
-            System.out.println("A" + page.getId());
-            byte[] id = "tt0000001".getBytes();
-            byte[] title = "Carmencita".getBytes();
-            Row r = new Row(id, title);
-            page.insertRow(r);
-            bm.unpinPage(page.getId());
-            System.out.println("got page " + i + ": " + bm.toString());
-        }
-
-        //now after we added some pages let's actually try writing something to them...
-        Page page = bm.getPage(0);
-        Row currRow = page.getRow(0);
-        System.out.println("HERE");
-        System.out.println(currRow.getMovieId());
-        System.out.println(currRow.getTitle());
-        
-        byte[] id = "tt0000001".getBytes();
-        byte[] title = "Carmencita".getBytes();
-        Row r = new Row(id, title);
-        page.insertRow(r);
-        System.out.println(page.getRow(1).getTitle());
-        bm.markDirty(0);
-        bm.unpinPage(0);
-        bm.createPage();
-        bm.createPage();
-        bm.createPage();
-        bm.createPage();
-        //this forces us to write to disk and we see it looks good YAY
-    }
 }
