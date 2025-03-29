309a1051c6bf5d97d12144530902873ee9f9e26b
Matthew Richardson <97010005+06tron@users.noreply.github.com>
2025-03-29 13:56:54 -0400

    Adds tree-index/build.xml

---
+++ tree-index/test/UnnamedTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class UnnamedTest {
+
+    @Test
+    public void exampleTest() {
+        assertTrue(2 < 3);;
+    }
+
+}
