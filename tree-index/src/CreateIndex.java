ea910e59d1fa5b25d46526fa6eae16c458402bea
tanviagarwal15 <tanviagarwal@umass.edu>
2025-04-01 10:33:29 -0400

    move index tests to tree-index

--- tree-index/src/CreateIndex.java
+++ tree-index/src/CreateIndex.java
@@ -2,17 +2,90 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.Random;
 
-public class Utilities {
+public class CreateIndex {
 
     public static void main(String[] args) {
         //experimented with different buffer sizes by changing Constants.BUFFER_SIZE
         BufferManager bufferManager = new LRUBufferManager();
-        loadDataset(bufferManager, Constants.IMDB_TSV_FILE);
+
+        loadDatasetSequentially(bufferManager, Constants.IMDB_TSV_FILE);
+
+        // TestC1 - create index on title
+        BTreeIndex<String> titleIndexBTree = IndexTests.CreateIndex(bufferManager, Constants.TITLE_INDEX);
+
+        // TestC3 - Verify index on title
+        IndexTests.verifyIndex(titleIndexBTree, bufferManager, "Boxing", Constants.TITLE_INDEX);
+
+        // TestC2 - create index on title
+        BTreeIndex<String> movieIdIndexBTree = IndexTests.CreateIndex(bufferManager, Constants.TITLE_INDEX);
+
+        // TestC3 - Verify index on title
+        IndexTests.verifyIndex(movieIdIndexBTree, bufferManager, "tt0000137", Constants.TITLE_INDEX);
+    }
+
+    public static void loadDatasetSequentially(BufferManager bf, String csvFile) {
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+            String curRow;
+            int pageId = 0;
+            Page currentPage = bf.createPage();
+            curRow = br.readLine(); // Skip title row
+            int pagesCreated = 0;
+            boolean skipLongMovieId = true;
+            int skippedMovies = 0;
+            int rowsProcessed = 0;
+
+            while (curRow != null) {
+                
+                if (currentPage.isFull()) {
+                    bf.unpinPage(currentPage.getId());
+
+                    currentPage = bf.createPage();
+                    pagesCreated++;
+                    pageId = currentPage.getId();
+                }
+
+                String[] columns = curRow.split("\t");
+                byte[] movieId = columns[0].getBytes();
+
+                if (skipLongMovieId && movieId.length > Constants.MOVIE_ID_SIZE) {
+                    skippedMovies++;
+                    curRow = br.readLine();
+                    continue;
+                }
+
+                byte[] title = columns[2].getBytes();
+                Row row = new Row(movieId, title);
+                int rowId = currentPage.insertRow(row);
+
+                // if row was inserted, mark the page dirty
+                if (rowId != -1) {
+                    rowsProcessed++;
+                    bf.markDirty(pageId);
+
+                    if (rowsProcessed % 1000000 == 0) {
+                        System.out.println("Processed row with ID " + rowId + " in page with Id " + currentPage.getId());
+                        System.out.println(rowsProcessed + " rows processed.");
+                    }
+
+                    curRow = br.readLine();
+                }
+            }
+
+            bf.unpinPage(currentPage.getId());
+            System.out.println(skippedMovies + " rows skipped due to long movieId.");
+            System.out.println("Processed " + rowsProcessed + " rows, Created " + pagesCreated + " pages.");
+            System.out.println("Finished loading dataset.");
+
+            bf.force();
+            System.out.println("Flushed to disk any remaining pages.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     // Loads the buffer manager with the imdb dataset
     // Interleaves row insertion with createPage and getPage methods
-    public static void loadDataset(BufferManager bf, String filepath) {
+    public static void loadInterleavedDataset(BufferManager bf, String filepath) {
         try (BufferedReader bufferReader = new BufferedReader(new FileReader(filepath))) {
             String curRow;
             int rowsProcessed = 0;
