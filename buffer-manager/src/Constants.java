1c6f01a81704f06bc5d497b70a9d9da0085a1dcc
Matthew Richardson <97010005+06tron@users.noreply.github.com>
2025-04-04 17:12:49 -0400

    Adjusted bm.Constants variable names

--- buffer-manager/src/Constants.java
+++ buffer-manager/src/Constants.java
@@ -2,8 +2,8 @@ public class Constants {
     public static final int PAGE_KB = 4;
     public static final int BUFFER_SIZE = 8; // Default buffer size
     public static final int IMDB_ROW_LENGTH = 39; // Row size in bytes
-    public static final int MOVIE_ID_SIZE = 9; // movieId size: 9
-    public static final int TITLE_SIZE = 30; // title size: 30
+    public static final int IMDB_MOVIE_ID_SIZE = 9; // movieId size: 9
+    public static final int IMDB_TITLE_SIZE = 30; // title size: 30
     public static final String DATA_DIRECTORY = "../data/";
     public static final String IMDB_TSV_FILE = DATA_DIRECTORY + "title.basics.tsv";
 }
