cff87e19885fe3bcc389367f2add6d82b63c37bf
Evan Ciccarelli <eciccarelli@umass.edu>
2025-04-04 12:40:48 -0400

    Working more on BTreeIndex through buffer manager

--- tree-index/src/BTreeIndexBM.java
+++ tree-index/src/BTreeIndexBM.java
@@ -6,24 +6,37 @@ import java.util.Arrays;
 
 public class BTreeIndexBM{
     //we are going to store (mostly) everything as byte arrays to make things easier
-    private BTreeNodeBM root; //root of the tree
+    private int maxKeysBytes; //used to denote how many bytes are set aside for storing maxKeys
+    private byte[] root; //root of the tree
+    private boolean rootIsLeaf; //true if the root node is a leaf node, false otherwise
     private int maxKeys; //max nos. of keys in node
     private BufferManager bm;
     private int rootPid = 0;
     private int rootSid = 0;
+    private Page rootPage;
+    private final int fixedLength = 15;
 
     public BTreeIndexBM(int maxKeys, BufferManager bm) {
-        this.root = new BTreeLeafNode<>();  //leaf node as the root at start
+        //each entry needs a k, i.e. the actual value the index is being sorted by
+        //a rid which is a page id and a slot id (an int, i.e. 4 bytes allocated for every page id and slot id should be sufficient)
+        //a previous pointer, again a page id and slot id
+        //a next pointer, again a page id and slot id
+
+        //for efficiency the first couple bytes of every row is going to be used to denote how full the current node is 
+        int numBitsFormaxKeys = 32 - Integer.numberOfLeadingZeros(maxKeys);
+        maxKeysBytes = (numBitsFormaxKeys + 7)/8; //for rounding down
+
+        this.root = new byte[maxKeysBytes+maxKeys*(fixedLength+3*8)];  //leaf node as the root at start, size should be
+        this.rootPage = bm.createPage("BTree", maxKeysBytes+maxKeys*(fixedLength+3*8));
+        this.rootPid = rootPage.getId();
+        this.rootSid = rootPage.insertRow(new Row(this.root));
+        rootIsLeaf = true;
         this.maxKeys = maxKeys;    //setting max keys for nodes
         this.bm = bm;
-        //for now let's say rowsize is maxKeys*15, 15 bytes for each key + maxKeys*1+1 for each pagereference.
-        //E.g. if maxKeys = 5 we need to store 5, 15 length strings, and 6 pageIds, one referencing those below the first string, one for each in between, and for each above the 5th
-        bm.createPage("BTree", 15*maxKeys+maxKeys+1);
-        //pageID should be 0
-        //might want to add somewhere a truncating function, for movieTitles for example, because in page we truncate whole rows where
-        //here a row can consist of multiple movietitles. Here is the function from another place...
-        /*
-        private byte[] toSize(byte[] arr, int size) {
+    }
+    //might want to add somewhere a truncating function, for movieTitles for example, because in page we truncate whole rows where
+    //here a row can consist of multiple movietitles. Here is the function from another place...
+    private byte[] toSize(byte[] arr, int size) {
         if (arr.length == size) {
             return arr;
         }
@@ -36,21 +49,44 @@ public class BTreeIndexBM{
         }
 
         return resized;
-         */
     }
+         
 
     public int getMaxKeys() {
         return maxKeys;
     }
 
-    public void insert(K key, Rid rid) {    //to insert a new key in tree
-        Row rootRow = bm.getPage(rootPid, "BTree").getRow(rootSid);
-        List<K> tempKeys = new ArrayList<>();
-        //this for loop is wrong right now
+    //new idea, ditch the classes they are just making it harder for me to think through, instead just have this file
+    //we want another private insert function which we will call for everything past the root, we can think of as recursing down
+    //but with a slightly different function
+    public void insert(byte[] key, Rid rid){
+        if(rootIsLeaf){
+            //in this case the root is just a leaf node, if there is room add to root
+            
+        }
+    }
+    //temp is just so the above is a different function because I want to think througb logic from scratch
+    public void insert(byte[] key, Rid rid, int temp) {    //to insert a new key in tree
+        Row rootRow = rootPage.getRow(rootSid);
+        //this for loop is looping through all the things in root, i.e. the keys and the values
         for(int i =0; i<maxKeys; i++){
             int start = i * 15;
             byte[] chunk = new byte[15];
             System.arraycopy(rootRow.data, start, chunk, 0, 15);
+            //checking to see if chunk is 0's, because if it is then there is no key there and we can stop
+            byte[] zeroArray = new byte[chunk.length]; // By default, this will be all zeros
+            if (Arrays.equals(chunk, zeroArray)){
+                break;
+            }
+            //otherwise we want to compare it, the below means if key <= chunk, LOOK INTO if it should be < or <=
+            if(Arrays.compare(key, chunk) <= 0){
+                //this means we are in the correct child
+                int childPos = 15*maxKeys+2*i;
+                //right now this is one byte storing this info but that might be too small
+                int childPageId = rootRow.data[childPos];
+                int childSlotId = rootRow.data[childPos+1];
+                //now call new helper function insert2()
+            }
             //Need to add a converter of some kind depedning on the type of the key (string, int etc.)
             //K tempKey = new Comparable(rootRow.data, start, 15, StandardCharsets.UTF_8).trim(); // Remove padding
             //tempKeys.add(tempKey);
