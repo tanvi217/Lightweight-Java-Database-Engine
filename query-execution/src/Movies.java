5b1628e6a47a3b7933bc2ed5ea1401488803cca6
Matthew Richardson <97010005+06tron@users.noreply.github.com>
2025-04-28 18:49:45 -0400

    Adds more schema classes

---
+++ query-execution/src/Movies.java
@@ -0,0 +1,5 @@
+// Movies (movieId: char(9), title: char(30))
+public class Movies {
+    public static int[] movieId = {0, 9};
+    public static int[] title = {9, 39};
+}
---
+++ query-execution/src/People.java
@@ -0,0 +1,5 @@
+// People (personId: char(10), name: char(105))
+public class People {
+    public static int[] personId = {0, 10};
+    public static int[] name = {10, 115};
+}
